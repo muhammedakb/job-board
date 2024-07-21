1373d8c776248970c93766709565ed7388946bc7
Muhammet Akbulut <muhammetakb68@gmail.com>
2024-07-21 17:44:57 +0300

    feat: authentication added from workOS

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,17 +1,41 @@
+import { getSignInUrl, getUser, signOut } from '@workos-inc/authkit-nextjs';
 import Link from 'next/link';
 
-export default function Header() {
+export default async function Header() {
+  const { user } = await getUser();
+  const signInUrl = await getSignInUrl();
+
   return (
     <header>
       <div className='container flex items-center justify-between mx-auto my-4'>
         <Link href='/' className='font-bold text-xl'>
           Job Board
         </Link>
-        <nav className='flex gap-4 *:py-2 *:px-4 *:rounded-md'>
-          <Link className='bg-gray-200' href='/login'>
-            Login
-          </Link>
-          <Link className='bg-blue-600 text-white' href='/new-listing'>
+        <nav className='flex gap-2'>
+          {!user && (
+            <Link className='bg-gray-200 rounded-md py-2 px-4' href={signInUrl}>
+              Login
+            </Link>
+          )}
+          {user && (
+            <form
+              action={async () => {
+                'use server';
+                await signOut();
+              }}
+            >
+              <button
+                type='submit'
+                className='bg-gray-200 rounded-md py-2 px-4'
+              >
+                Logout
+              </button>
+            </form>
+          )}
+          <Link
+            className='bg-blue-600 text-white rounded-md py-2 px-4'
+            href='/new-listing'
+          >
             Post a job
           </Link>
         </nav>
